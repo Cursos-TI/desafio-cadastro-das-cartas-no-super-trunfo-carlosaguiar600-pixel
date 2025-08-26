051732607a1db3ef1511a9ab3e2a38f0ccac162c
carlosaguiar600-pixel <carlosaguiar600@gmail.com>
2025-08-26 19:37:09 +0000

    criei CartasSuperTrunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,55 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
 
-int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+int main()
+{
+
+//Identificação dos critérios da carta 1
+
+    char super_trunfo;
+    int Carta = 1;
+    char Estado = '1';
+    char Codigo = '1';
+    char Cidade = '1';
+    int Populacao = 1;
+    float Area = 1;
+    float PIB = 1;
+    int Pontos_turisticos = 1;
+
+
+// impressão dos atribotos da carta 1
+    printf(" DESAFIO SUPER TRUNFO!! %c\n", super_trunfo);
+    printf("A Carta: %d\n\n", Carta);
+    printf("Estado: %c\n", Estado);
+    printf("Codigo: %c\n", Codigo);
+    printf("Cidade: %c\n", Cidade);
+    printf("Populacao: %d\n", Populacao);
+    printf("area: %.0f\n", Area);
+    printf("PIB: %.0f\n", PIB);
+    printf("Pontos turisticos: %d\n\n\n\n\n", Pontos_turisticos);
+
+
+    //Identificação dos critérios da carta 2
+    int Carta_2 = 2;
+    char Estado_2 = '2';
+    char Codigo_2 = '2';
+    char Cidade_2 = '2';
+    int Populacao_2 = 2;
+    float Area_2 = 2;
+    float PIB_2 = 2;
+    int Pontos_turisticos2 = 2;
+
+// impressão dos atribotos da carta 2
+    printf("A Carta: %d\n\n", Carta_2);
+    printf("Estado: %c\n", Estado_2);
+    printf("Codigo: %c\n", Codigo_2);
+    printf("Cidade: %c\n", Cidade_2);
+    printf("Populacao: %d\n", Populacao_2);
+    printf("area: %.0f\n", Area_2);
+    printf("PIB: %.0f\n", PIB_2);
+    printf("Pontos turisticos: %d\n", Pontos_turisticos2);
+
 
     return 0;
 }
